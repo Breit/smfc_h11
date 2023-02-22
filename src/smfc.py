1b5d755cf1c5ae92c1753d6e61ec90a57cd51ced
Breit <breit@gammagoblins.de>
2023-02-22 01:29:28 +0100

    WIP

--- src/smfc.py
+++ src/smfc.py
@@ -3,6 +3,8 @@
 #   smfc.py (C) 2020-2022, Peter Sulyok
 #   IPMI fan controller for Super Micro X9/X10/X11 motherboards.
 #
+#   modified for Supermicro H11SSL-* by Martin Breitbarth, 2023
+#
 import argparse
 import configparser
 import glob
@@ -747,97 +749,7 @@ class HdZone(FanController):
             if len(self.hwmon_path) != self.count:
                 raise ValueError(f'Invalid hd_names= parameter, not all hwmon files was found ({self.hwmon_path})')
 
-    def callback_func(self) -> None:
-        """Call-back function execute standby guard."""
-        if self.standby_guard_enabled:
-            self.run_standby_guard()
-
-    def get_standby_state_str(self) -> str:
-        """Get a string representing the power state of the HD array with a character.
-
-        Returns:
-            str:   standby state string where all HD represented with a character (A-ACTIVE, S-STANDBY)
-        """
-        result: str = ''    # Result string
-
-        for i in range(self.count):
-            if self.standby_array_states[i]:
-                result += 'S'
-            else:
-                result += 'A'
-        return result
-
-    def check_standby_state(self):
-        """Check the actual power state of the HDs in the array and store them in 'standby_states'.
-
-        Returns:
-            int:   number of HDs in STANDBY mode
-        """
-        r: subprocess.CompletedProcess      # Result of the executed process.
-
-        # Check the current power state of the HDs
-        for i in range(self.count):
-            self.standby_array_states[i] = False
-            r = subprocess.run([self.smartctl_path, '-i', '-n', 'standby', self.hd_device_names[i]],
-                               check=False, capture_output=True, text=True)
-            if r.returncode not in {0, 2}:
-                raise ValueError(self.ERROR_MSG_SMARTCTL.format(r.returncode))
-            if str(r.stdout).find("STANDBY") != -1:
-                self.standby_array_states[i] = True
-        return self.standby_array_states.count(True)
-
-    def go_standby_state(self):
-        """Put active HDs to STANDBY state in the array (based on the actual state of 'standby_states').
-        """
-        r: subprocess.CompletedProcess      # Result of the executed process.
-
-        # Iterate through HDs list
-        for i in range(self.count):
-            # if the HD is ACTIVE
-            if not self.standby_array_states[i]:
-                # then move it to STANDBY state
-                r = subprocess.run([self.smartctl_path, '-s', 'standby,now', self.hd_device_names[i]],
-                                   check=False, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
-                if r.returncode != 0:
-                    raise ValueError(self.ERROR_MSG_SMARTCTL.format(r.returncode))
-                self.standby_array_states[i] = True
-
-    def run_standby_guard(self):
-        """Monitor changes in the power state of an HD array and help them to move to STANDBY state together.
-        This feature is implemented in the following way:
-            * step 1: Checks the power state of all HDs in array
-            * step 2: Check if the array is going to STANDBY (i.e. was ACTIVE before and reached the limit with number
-                      of HDs in STANDBY state), put the remaining active members to STANDBY state and log the event
-            * step 3: Check if the array is waking up (i.e. was in STANDBY state before and there is any ACTIVE
-                      HD(s) in the array) and log the event
-        """
-        hds_in_standby: int     # HDs in standby mode
-        minutes: float          # Elapsed time in minutes
-        cur_time: float         # New timestamp for STANDBY change
-
-        # Step 1: check the current power state of the HD array
-        hds_in_standby = self.check_standby_state()
-        cur_time = time.monotonic()
-
-        # Step 2: check if the array is going to STANDBY state.
-        if not self.standby_flag and hds_in_standby >= self.standby_hd_limit:
-            minutes = (cur_time - self.standby_change_timestamp) / float(3600)
-            self.log.msg(self.log.LOG_INFO, f'Standby guard: Change ACTIVE to STANDBY after {minutes:.1f} hour(s)'
-                         f'[{self.get_standby_state_str()}]')
-            self.go_standby_state()
-            self.standby_flag = True
-            self.standby_change_timestamp = cur_time
-
-        # Step 3: check if the array is waking up.
-        elif self.standby_flag and hds_in_standby < self.count:
-            minutes = (cur_time - self.standby_change_timestamp) / float(3600)
-            self.log.msg(self.log.LOG_INFO, f'Standby guard: Change STANDBY to ACTIVE after {minutes:.1f} hour(s)'
-                         f'[{self.get_standby_state_str()}]')
-            self.standby_flag = False
-            self.standby_change_timestamp = cur_time
-
-
-def main():
+ def main():
     """Main function: starting point of the systemd service."""
     my_parser: argparse.ArgumentParser      # Instance for an ArgumentParser class
     my_results: argparse.Namespace          # Results of parsed command line arguments
